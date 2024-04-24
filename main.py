51956bd0dbe4436834c05b76a8d533736455942a
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-04-24 15:28:12 +0100

    added status highlighting + list caching
    ooh fancy

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import requests
 import json
 import os
+import sys
 import shutil
 import zipfile
 import time
-from pathlib import Path
 
 #Define variables
 listURL = "https://wspm.pages.dev/package-list"
 baseURL = "https://wspm.pages.dev/packages/"
+
+GREEN = '\033[38;5;120m'
+RED = '\033[38;5;203m'
+BLUE = '\033[38;5;117m' #dark-aqua sort of colour
 
 if os.name == "nt":
     installdir = os.getenv('LOCALAPPDATA') + "\\wspm"
@@ -26,11 +30,18 @@ else:
 packagedir = os.path.join(installdir, "packages")
 packageListDir = os.path.join(installdir, "package-list")
 
-def pront(stufftoprint):
+def producesyntaxed(text, color='\033[38;5;120m'):
+    try:
+        sys.stdout.write(color + text + '\033[0m')
+    except:
+        print(text)
+
+def pront(stufftoprint, colour=BLUE):
+    stufftoprint = str(stufftoprint) + "\n"
     if __name__ == "__main__":
-        print(stufftoprint)
+        producesyntaxed(stufftoprint, colour)
     else:
-        print(f"{__name__}: {stufftoprint}")
+        producesyntaxed(f"{__name__}: {stufftoprint}", BLUE)
 
 def metaSave(path, data):
     data = str(data).replace("'", '"')
@@ -42,13 +53,17 @@ def metaSave(path, data):
         with open(os.path.join(path, "metadata"), 'w') as f:
             f.write(data)
 
-def saveFile(path, name, data):
+def saveFile(path, data, name=None):
+    if name is not None:
+        directory = os.path.join(path, name)
+    else:
+        directory = path
     try:
-        with open(os.path.join(path, name), 'wb') as f:
+        with open(directory, 'wb') as f:
             f.write(data)
     except OSError:
         os.mkdir(path)
-        with open(os.path.join(path, name), 'wb') as f:
+        with open(directory, 'wb') as f:
             f.write(data)
 
 def readCurrentVersion(packageName: str) -> dict:
@@ -66,15 +81,16 @@ def download_file(url: str):
         if response.status_code == 200:
             return response.content
         else:
-            pront("Failed to download file.")
+            pront("Failed to download file.", RED)
     except requests.exceptions.ConnectionError:
         print("Failed to connect. Maybe check your internet connection?")
+        return None
 
 def getMetadata(name: str) -> list:
     return json.loads(download_file(f"{baseURL}{name}/metadata"))
 
 def extract(packageName):
-    print("Extracting " + packageName)
+    pront("Extracting " + packageName, BLUE)
     path = os.path.join(packagedir, packageName)
     files = os.listdir(path)
     for file in files:
@@ -82,7 +98,7 @@ def extract(packageName):
             file_path = os.path.join(path, file)
             with zipfile.ZipFile(file_path, 'r') as zip_ref:
                 zip_ref.extractall(path)
-                print("Extracted "+ file_path)
+                pront("Extracted "+ file_path, GREEN)
                 os.remove(file_path)
 
 
@@ -95,15 +111,14 @@ def installp2(metadata, packageName):
         metaSave(os.path.join(packagedir, packageName), metadata)
         for file in files:
             dl = download_file(f"{baseURL}{packageName}/{file}")
-            saveFile(os.path.join(packagedir, packageName), file, dl)
+            saveFile(os.path.join(packagedir, packageName), dl, file)
         extract(packageName)
-        pront("Installation success!")
     except Exception as e:
-        pront("Installation failed.\n " + str(e))
+        pront("Installation failed.\n " + str(e), RED)
 
 def installDeps(deps):
-    pront("Found dependencies!")
-    pront(deps)
+    pront("Found dependencies!", GREEN)
+    pront(f"Dependencies: {deps}", BLUE)
     deps = deps.split(", ")
     for dep in deps:
         pront("Downloading " + dep)
@@ -111,7 +126,7 @@ def installDeps(deps):
         if float(metadata["version"]) > float(readCurrentVersion(dep)):
             installp2(metadata, dep)
         else:
-            pront(f"Dependency {dep} is already up to date")
+            pront(f"Dependency {dep} is already up to date", GREEN)
 
 def install(packageName, packages):
     if packageName in packages:
@@ -120,7 +135,7 @@ def install(packageName, packages):
             metadata = getMetadata(packageName)
             deps = metadata['dependencies']
             if not float(metadata["version"]) > float(readCurrentVersion(packageName)):
-                pront(f"Package {packageName} is already up to date.")
+                pront(f"Package {packageName} is already up to date.", GREEN)
                 if deps !="":
                     depUpdate = input("However, dependencies were found. Do you want to update dependencies? (Y/N): ").lower()
                     if depUpdate == "y":
@@ -130,35 +145,55 @@ def install(packageName, packages):
                 installp2(metadata, packageName)
             else:
                 installp2(metadata, packageName)
+            pront(f"Installation of package {packageName} success!", GREEN)
         except Exception as e:
             match e:
                 case json.decoder.JSONDecodeError:
-                    pront("Not found or some other JSON ded")
-                    pront("Abort")
+                    pront("Not found or some other JSON ded", RED)
+                    pront("Abort", RED)
                 case _:
-                    pront(e)
+                    pront(e, RED)
     else:
-        pront(f"Package {packageName} does not exist")
+        pront(f"Package {packageName} does not exist", RED)
 
 def deletePackage(packageName):
     pront("Removing " + packageName)
     try:
         shutil.rmtree(os.path.join(packagedir, packageName))
-        pront("Remove success!")
+        pront("Remove success!", GREEN)
     except Exception as e:
-        pront("Failed")
-        pront(e)
+        pront("Failed", RED)
+        pront(e, RED)
 
 def remove(packageName):
     if input(f"Are you sure you want to remove {packageName}? (Y/N) ").lower() == "y":
         deletePackage(packageName)
     else:
-        pront("Abort")
+        pront("Abort", RED)
 
+
+def checkCache(file_path, fileURL=listURL):
+    if os.path.exists(file_path):
+        # Get the last modification time of the file
+        last_modified = os.path.getmtime(file_path)
+        # Get the current time
+        current_time = time.time()
+        # Calculate the difference in seconds
+        time_difference = current_time - last_modified
+        # Check if the file has been updated in the last hour (3600 seconds)
+        if time_difference > 3600:
+            data = download_file(f"{fileURL}")
+            saveFile(file_path, data)
+    else:
+        data = download_file(f"{fileURL}")
+        saveFile(file_path, data)
+    
+    with open(packageListDir, "rb") as f:
+        return f.read()
 
 def main():
-    packages = str(download_file(listURL)).removeprefix("b").replace("'", "").split(", ")
-
+    plainPackageStr = checkCache(packageListDir)
+    packages = str(plainPackageStr).removeprefix("b").replace("'", "").split(", ")
     command = input("Type a command\n")
     packageNames = input("Type a package/packages\n").split(" ")
     match command:
